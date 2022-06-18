ca231f4412ff67d2021a0f7ae829262f83def1ad
Kanapin Marat <kanapinmarat@yandex.ru>
2022-06-18 11:32:19 +0300

    fix: поправить джаваскрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,51 +1,38 @@
 const edit = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const sumbit = document.querySelector('.popup__save-button');
+const popupForm = document.querySelector('.popup__form');
 const popupCloseButton = document.querySelector('.popup__close-button');
-const inputName = document.querySelector('.popup__field_name');
-const inputInterest = document.querySelector('.popup__field_interest');
+const inputName = document.querySelector('.popup__input_name');
+const inputInterest = document.querySelector('.popup__input_interest');
 const profileName = document.querySelector('.profile__name');
 const profileInterest = document.querySelector('.profile__interest');
-
-function editProfileOnEnter(e) {
-    if (e.key === 'Enter') {
-        editProfile();
-    }
-    console.log(1);
-}
-
-function editProfile() {
-    profileName.textContent = inputName.value;
-    profileInterest.textContent = inputInterest.value;
-    document.addEventListener('keydown', editProfileOnEnter);
-    closePopup();
-}
+const save = ('.popup__save-button');
 
 
 function openPopup() {
-    popup.classList.remove('popup_hidden');
-    document.addEventListener('keydown', closePopupOnEsc);
+    popup.classList.add('popup_open');
 }
 
 function closePopup() {
-    popup.classList.add('popup_hidden');
-    document.removeEventListener('keydown', closePopupOnEsc);
+    popup.classList.remove('popup_open');
 }
 
-function closePopupOnEsc(e) {
-    if (e.key === 'Escape') {
-        closePopup();
-    }
-    console.log(1);
-}
-
-edit.addEventListener('click', function () {
+function editProfile() {
     openPopup();
-});
+    inputName.value = profileName.textContent;
+    inputInterest.value = profileInterest.textContent;
+}
 
-popupCloseButton.addEventListener('click', function () {
+function sumbitForm(evt) {
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileInterest.textContent = inputInterest.value;
     closePopup();
-});
+}
+
+edit.addEventListener('click', editProfile);
+
+popupCloseButton.addEventListener('click', closePopup);
 
 popup.addEventListener('click', function (e) {
     if (e.target === e.currentTarget) {
@@ -53,7 +40,4 @@ popup.addEventListener('click', function (e) {
     }
 });
 
-sumbit.addEventListener('click', function () {
-    editProfile();
-});
-
+popupForm.addEventListener('sumbit', sumbitForm);
